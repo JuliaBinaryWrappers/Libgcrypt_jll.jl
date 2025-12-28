ee6f99bff014fbb3a3a1f8d5a68a3707ec6d1cfa
jlbuild <juliabuildbot@gmail.com>
2025-12-28 21:48:33 +0000

    Libgcrypt_jll build 1.11.2+0

--- src/Libgcrypt_jll.jl
+++ src/Libgcrypt_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Libgcrypt")
-JLLWrappers.@generate_main_file("Libgcrypt", UUID("d4300ac3-e22c-5743-9152-c294e39db1e4"))
+JLLWrappers.@generate_main_file("Libgcrypt", Base.UUID("d4300ac3-e22c-5743-9152-c294e39db1e4"))
 end  # module Libgcrypt_jll
